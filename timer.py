3c37fb73a10ff0ceec5665d22d181bbe82d71e1b
tomekd85 <tomekdomachowski@wp.pl>
2022-04-30 23:24:31 +0200

    Create input from GPIO buttons.

--- timer.py
+++ timer.py
@@ -2,16 +2,16 @@ import os
 import time
 
 from Display.OledDisplay.OledDisplay import OledDisplay
-from countdowntimer.ButtonInput import ButtonInput
-from countdowntimer.KeyboardInput import KeyboardInput
 from countdowntimer.CountDownTimer import CountDownTimer
 
 
 if __name__ == '__main__':
     oled = OledDisplay()
     if os.uname().machine.startswith("arm"):
+        from countdowntimer.ButtonInput import ButtonInput
         button_input = ButtonInput()
     else:
+        from countdowntimer.KeyboardInput import KeyboardInput
         button_input = KeyboardInput()
     timer = CountDownTimer(oled, button_input, 20)
     timer.count()
